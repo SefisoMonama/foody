ae1e4de8759f87472188c820dc5cf0ee9fa1f7c5
SefisoMonama <sfisomonama998@gmail.com>
2021-01-21 20:45:44 +0200

    add bottom navigation bar

--- app/src/main/java/com/strixtechnology/foody/MainActivity.kt
+++ app/src/main/java/com/strixtechnology/foody/MainActivity.kt
@@ -2,16 +2,40 @@ package com.strixtechnology.foody
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.NavHost
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
-import com.strixtechnology.foody.dataBinding.ActivityMainBinding
 
-private lateinit var binding: ActivityMainBinding
+//import com.strixtechnology.foody.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+    //private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        //binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(R.layout.activity_main)
+
+        navController = findNavController(R.id.navHostFragment)
+        val appBarConfiguration: AppBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.recipesFragment,
+                R.id.favoriteRecipesFragment,
+                R.id.foodJokeFragment
+            )
+        )
+        bottomNavigation.setupWithNavController(navController)
+        setupActionBarWithNavController(navController,appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
