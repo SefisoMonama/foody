486b7c687104e2cf375f6631b40ae67059231aa7
SefisoMonama <sfisomonama998@gmail.com>
2021-02-01 17:33:12 +0200

    add bottomSheet Filter

--- app/src/main/java/com/strixtechnology/foody/ui/fragments/recipes/RecipesFragment.kt
+++ app/src/main/java/com/strixtechnology/foody/ui/fragments/recipes/RecipesFragment.kt
@@ -9,6 +9,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.strixtechnology.foody.viewmodels.MainViewModel
 import com.strixtechnology.foody.R
@@ -51,6 +52,11 @@ class RecipesFragment : Fragment() {
 
         setupRecyclerView()
         readDatabase()
+
+        binding.recipesFab.setOnClickListener{
+            findNavController().navigate(R.id.action_recipesFragment_to_recipesBottomSheet)
+        }
+
 
         return binding.root
     }
--- app/src/main/java/com/strixtechnology/foody/viewmodels/RecipesViewModel.kt
+++ app/src/main/java/com/strixtechnology/foody/viewmodels/RecipesViewModel.kt
@@ -1,25 +1,56 @@
 package com.strixtechnology.foody.viewmodels
 
 import android.app.Application
+import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.strixtechnology.foody.data.DataStoreRepository
+import com.strixtechnology.foody.data.MealAndDietType
 import com.strixtechnology.foody.util.Constants
 import com.strixtechnology.foody.util.Constants.Companion.API_KEY
+import com.strixtechnology.foody.util.Constants.Companion.DEFAULT_DIET_TYPE
+import com.strixtechnology.foody.util.Constants.Companion.DEFAULT_MEAL_TYPE
+import com.strixtechnology.foody.util.Constants.Companion.DEFAULT_RECIPES_NUMBER
 import com.strixtechnology.foody.util.Constants.Companion.QUERY_ADD_RECIPE_INFORMATION
 import com.strixtechnology.foody.util.Constants.Companion.QUERY_API_KEY
 import com.strixtechnology.foody.util.Constants.Companion.QUERY_DIET
 import com.strixtechnology.foody.util.Constants.Companion.QUERY_FILL_INGREDIENTS
 import com.strixtechnology.foody.util.Constants.Companion.QUERY_NUMBER
 import com.strixtechnology.foody.util.Constants.Companion.QUERY_TYPE
+import dagger.hilt.android.scopes.ActivityRetainedScoped
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
-class RecipesViewModel(application: Application): AndroidViewModel(application){
+@ActivityRetainedScoped
+class RecipesViewModel @ViewModelInject constructor(
+    application: Application,
+    private val dataStoreRepository: DataStoreRepository
+): AndroidViewModel(application){
 
+    private var mealType = DEFAULT_MEAL_TYPE
+    private var dietType = DEFAULT_DIET_TYPE
+
+    val readMealAndDietType =  dataStoreRepository.readMealAndDietType
+
+    fun saveMealAndDiet(mealType: String, mealTypeId: Int, dietType: String, dietTypeId: Int) =
+        viewModelScope.launch(Dispatchers.IO){
+        dataStoreRepository.saveMealAndDietType(mealType, mealTypeId, dietType, dietTypeId)
+    }
     fun applyQueries(): HashMap<String, String>{
         val queries: HashMap<String, String> = HashMap()
 
-        queries[QUERY_NUMBER] = "50"
+        viewModelScope.launch {
+            readMealAndDietType.collect { value ->
+                mealType = value.selectedMealType
+                dietType = value.selectedDietType
+            }
+        }
+
+        queries[QUERY_NUMBER] = DEFAULT_RECIPES_NUMBER
         queries[QUERY_API_KEY] = API_KEY
-        queries[QUERY_TYPE] = "beverage"
-        queries[QUERY_DIET] =  "vegan"
+        queries[QUERY_TYPE] = mealType
+        queries[QUERY_DIET] =  dietType
         queries[QUERY_ADD_RECIPE_INFORMATION] = "true"
         queries[QUERY_FILL_INGREDIENTS] = "true"
 
