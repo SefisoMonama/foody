ec08cb2ec851178f673432ad190594b012f8f02d
SefisoMonama <sfisomonama998@gmail.com>
2021-02-08 15:33:50 +0200

    modify Instructions fragment

--- app/src/main/java/com/strixtechnology/foody/ui/fragments/instructions/InstructionsFragment.kt
+++ app/src/main/java/com/strixtechnology/foody/ui/fragments/instructions/InstructionsFragment.kt
@@ -5,7 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebViewClient
 import com.strixtechnology.foody.R
+import com.strixtechnology.foody.models.Result
+import com.strixtechnology.foody.util.Constants
+import kotlinx.android.synthetic.main.fragment_instructions.view.*
+
 class instructionsFragment : Fragment() {
 
     override fun onCreateView(
@@ -13,6 +18,14 @@ class instructionsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_instructions, container, false)
+        val view = inflater.inflate(R.layout.fragment_instructions, container, false)
+
+        val args = arguments
+        val myBundle: Result? = args?.getParcelable(Constants.RECIPE_RESULT_KEY)
+
+        view.instructions_webView.webViewClient = object: WebViewClient(){}
+        val websiteUrl: String = myBundle!!.sourceUrl
+        view.instructions_webView.loadUrl(websiteUrl)
+        return view
     }
 }
